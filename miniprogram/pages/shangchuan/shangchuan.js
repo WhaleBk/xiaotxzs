171aa483d4e3dd06b521f15e568032c504166141
zhangjingyu <2102789582@qq.com>
2022-01-21 02:36:42 +0800

    图文发布功能已经实现

--- miniprogram/pages/shangchuan/shangchuan.js
+++ miniprogram/pages/shangchuan/shangchuan.js
@@ -1,17 +1,19 @@
 
-
-var imgurl=''
+var filePath=''
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    myValue:''
+    myValue:'',
+    imgurl:''
   },
   test:function(){
   
     console.log(this.data.myValue)
+   
+    console.log(this.data.imgurl)
   },
  
   CUImage2() {
@@ -23,46 +25,81 @@ Page({
       sizeType: ['compressed'],
       sourceType: ['album', 'camera'],
       success: res=> {
-        var filePath = res.tempFilePaths[0]
+         filePath = res.tempFilePaths[0]
             this.setData({
              
               imgurl:filePath 
             })
-          
-        const cloudPath = `index/${Date.now()}-${Math.floor(Math.random(0,1)*1000)}`+filePath.match(/\.[^.]+?$/)[0]
-        wx.cloud.uploadFile({
-          cloudPath,
-          filePath,
-          success: res => {
-            console.log("云存储上传成功", res)
-            const db = wx.cloud.database()
-            const name = `index-${Date.now()}-${Math.floor(Math.random(0,1)*1000)}`+filePath.match(/\.[^.]+?$/)[0]
-            const fileID = res.fileID
-            this.setData({
-              fileID:fileID,
-              imgurl:fileID
-            })
-            db.collection('tuwenxinxi').add({
-              data: {
-                name: name,
-                content: content,
-                imgurl: fileID,
-              }
-            })
-            .then(result => {
-              console.log("数据库写入成功", result)
+         
+         
+        // const cloudPath = `index/${Date.now()}-${Math.floor(Math.random(0,1)*1000)}`+filePath.match(/\.[^.]+?$/)[0]
+        // wx.cloud.uploadFile({
+        //   cloudPath,
+        //   filePath,
+        //   success: res => {
+        //     console.log("云存储上传成功", res)
+        //     const db = wx.cloud.database()
+        //     const name = `index-${Date.now()}-${Math.floor(Math.random(0,1)*1000)}`+filePath.match(/\.[^.]+?$/)[0]
+        //     const fileID = res.fileID
+        //     this.setData({
+        //       fileID:fileID,
+        //       imgurl:fileID
+        //     })
+        //     db.collection('tuwenxinxi').add({
+        //       data: {
+        //         name: name,
+        //         content: content,
+        //         imgurl: fileID,
+        //       }
+        //     })
+        //     .then(result => {
+        //       console.log("数据库写入成功", result)
               
-            })
-            .catch(err => {
-              console.error("数据库写入失败", err)
-            })}})
+        //     })
+        //     .catch(err => {
+        //       console.error("数据库写入失败", err)
+        //     })}})
           },
         })
       },
   
-
        
       
+  CUImage1() {
+    const content=this.data.myValue
+    console.log(content)
+    const filePath=this.data.imgurl
+    console.log(filePath)
+    const cloudPath = `index/${Date.now()}-${Math.floor(Math.random(0,1)*1000)}`+filePath.match(/\.[^.]+?$/)[0]
+    wx.cloud.uploadFile({
+      cloudPath,
+      filePath,
+      success: res => {
+        console.log("云存储上传成功", res)
+        const db = wx.cloud.database()
+        const name = `index-${Date.now()}-${Math.floor(Math.random(0,1)*1000)}`+filePath.match(/\.[^.]+?$/)[0]
+        const fileID = res.fileID
+        this.setData({
+          fileID:fileID,
+          imgurl:fileID
+        })
+        db.collection('tuwenxinxi').add({
+          data: {
+            name: name,
+            content: content,
+            imgurl: fileID,
+          }
+        })
+        .then(result => {
+          console.log("数据库写入成功", result)
+          
+        })
+        .catch(err => {
+          console.error("数据库写入失败", err)
+        })}})
+   
+
+      },
   /**
    * 生命周期函数--监听页面加载
    */
